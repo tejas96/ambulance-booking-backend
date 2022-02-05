f5047e8a1d99dfd89d9904d8b49077cd823d18a0
tejas96 <tejas96patil@gmail.com>
2022-02-05 21:10:48 +0530

    add body parser

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import app, { initRoutes } from './routes';
+import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+
+// create application/json parser
+var jsonParser = bodyParser.json();
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// apply the body parser middleware to all incoming requests
+app.use(jsonParser);
+app.use(urlencodedParser);
 
 dotenv.config();
 initRoutes();
